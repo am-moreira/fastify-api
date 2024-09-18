f43a7152c97532b30ded95047691f8876adda624
am-moreira <ammorei@gmail.com>
2024-09-18 08:48:23 -0300

    modularizando routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
+import routes from "./routes";
 import client from './config/db'
 
 const app = fastify({ logger: true })
+
+app.register(routes)
 
 export default app
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,22 @@
-import app from '../app'
+// import app from '../app'
+import { FastifyInstance } from 'fastify';
 import client from '../config/db'
 
-app.get('/', () => {
+async function routes(app: FastifyInstance) {
+  app.get('/', (request, reply) => {
     return 'AVALIAÇÕES'
-})
+  })
 
-app.get('/avaliacoes', async (req, reply) => {
+  app.get('/avaliacoes', async (request, reply) => {
     try {
       // Executa a consulta SELECT
       const result = await client.query('SELECT * FROM avaliacoes');
-      return result.rows; // Retorna os dados
+      reply.send(result.rows); // Retorna os dados
     } catch (error) {
       reply.status(500).send({ error: 'Erro ao buscar as avaliações' });
     }
-});
+  });
+}
+
+export default routes
+
